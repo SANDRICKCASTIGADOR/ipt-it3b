6d519e3205e20f734f84751fe5a52edacec5349f
SandrickCastigador <sandrickcastigador@gmail.com>
2025-07-17 09:26:06 +0800

    Implemented API Creation and AP

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import { db } from "~/server/db";
+import { users } from "~/server/db/schema";
+
+export async function GET() {
+   try {
+      const allUsers = await db.select().from(users);
+      return NextResponse.json(allUsers, { status: 200});
+   } catch (error) {
+      console.error("Error to fetch users");
+      return NextResponse.json(
+         {error: "Failed to fetch users" },
+         { status: 500}
+      );
+
+   }
+}
